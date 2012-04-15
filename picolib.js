721e288965a00621dbcfb56336dc51f2ee00db88
Matt Claypotch <thepotch@gmail.com>
2012-04-14 17:18:07 -0700

    stop using .bind for iOS :( add in fmt, tiny string interpolation function.

--- picolib.js
+++ picolib.js
@@ -1,41 +1,66 @@
-var $ = (function(win, doc, undefined) {
+        var $ = (function(win, doc, undefined) {
 
     function pico(sel) {
         var ret,
+            p,
             forEach = Array.prototype.forEach;
 
         ret = sel.nodeType ? [sel] : doc.querySelectorAll(sel);
 
-        ret.each = (function(fn) {
-            forEach.call(this, function(item) {
+        ret.each = function(fn) {
+            forEach.call(ret, function(item) {
                 fn.call(item);
             });
-            return this;
-        }).bind(ret);
+            return ret;
+        };
 
+        console.log('foo');
 
-        ret.on = (function(type, handler) {
-            this.each(function() {
+        ret.on = function(type, handler) {
+            ret.each(function() {
                 on(this, type, handler)
             });
-            return this;
-        }).bind(ret);
+            return ret;
+        };
 
 
-        ret.css = (function(o) {
+        ret.css = function(o) {
             if (typeof o == 'object') {
                 for (p in o) {
-                    this.each(function() {
+                    ret.each(function() {
                         this.style[p] = o[p];
                     });
                 }
-                return this;
+                return ret;
             }
-            return win.getComputedStyle(this[0]).getPropertyValue(o);
-        }).bind(ret);
+            return win.getComputedStyle(ret[0]).getPropertyValue(o);
+        };
+
+
+        ret.attr = function(o) {
+            if (typeof o == 'object') {
+                for (p in o) {
+                    ret.each(function() {
+                        this.setAttribute(p, o[p]);
+                    });
+                }
+                return ret;
+            }
+            return ret[0].getAttribute(o);
+        };
 
 
         return ret;
+    };
+
+    var fmt_re = /\{([^}]+)\}/g;
+    var fmt = pico.fmt = function() {
+        return function(s, args) {
+            if (!args) return;
+            if (!(args instanceof Array || args instanceof Object))
+                args = Array.prototype.slice.call(arguments, 1);
+            return s.replace(fmt_re, function(_, match){ return args[match]; });
+        };
     };
 
     var on = pico.on = function(el, type, handler) {
@@ -43,6 +68,7 @@ var $ = (function(win, doc, undefined) {
             handler.call(e.target, e);
         }, false);
     };
+
 
     return pico;
 })(window, document);
