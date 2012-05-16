b56d559c252f51c7a5f71c455a95b1d3ec1d0d62
Matt Claypotch <thepotch@gmail.com>
2012-05-15 17:08:22 -0700

    fix some buggy stuff in µ.def

--- µ.def.js
+++ µ.def.js
@@ -1,57 +1,60 @@
 // Simple deferred implementation.
 // Usage:
-// var def = new µ.def();
+// var def = new mu.def();
 // def.onpass(yay).onpass(dance).onfail(boo);
 // def.pass('party'); // yay() and dance() are called with 'party'
 
-µ.def = function() {
-    var fn = {},
-        that = this;
-    ['pass','fail'].forEach(function(m) {
-        fn[m] = [];
-        that['on'+m] = function(f) {
-            fn[m].push(f);
-            return that;
-        };
-        that[m] = function() {
-            var params = µ.arr(arguments);
-            fn[m].forEach(function(f) {
-                f.apply(false,params);
-            });
-            that.result = m;
-        };
-    });
-};
+(function(mu, len) {
+    mu.def = function() {
+        var fn = {},
+            that = this;
+        ['pass','fail'].forEach(function(m) {
+            fn[m] = [];
+            that['on'+m] = function(f) {
+                fn[m].push(f);
+                return that;
+            };
+            that[m] = function(params) {
+                if (that.result) return;
+                params = mu.arr(arguments);
+                fn[m].forEach(function(f) {
+                    f.apply(false,params);
+                });
+                that.result = m;
+            };
+        });
+    };
 
-µ.when = function() {
-    var ret = new µ.def(),
-        defs = µ.arr(arguments),
-        vals = [],
-        left = defs.length;
-    function ph(n) {
-        return function(o) {
-            if (arguments.length > 1) {
-                vals[n] = µ.arr(arguments);
-            } else {
-                vals[n] = o;
-            }
-            if (--left < 1) {
-                ret.pass(vals);
-            }
-        };
-    }
-    function fh() {
-        ret.fail(arguments);
-    }
-    setTimeout(function() {
-        for (var i=0; i<defs.length; i++) {
-            var d = defs[i];
-            if (d instanceof µ.def) {
-                d.onpass(ph(i)).onfail(fh);
-            } else {
-                ph(i)(d);
-            }
+    mu.when = function() {
+        var ret = new mu.def(),
+            defs = mu.arr(arguments),
+            vals = [],
+            a,
+            left = defs[len];
+        function ph(n) {
+            return function(o) {
+                a = arguments;
+                vals[n] = a[len] > 1 ? mu.arr(a) : o;
+                if (--left < 1) {
+                    ret.pass.apply(false,vals);
+                }
+            };
         }
-    }, 0);
-    return ret;
-}
+        function fh() {
+            ret.fail(arguments);
+        }
+        setTimeout(function() {
+            var i,d;
+            for (i=0; i<defs[len]; i++) {
+                d = defs[i];
+                if (d instanceof mu.def) {
+                    d.onpass(ph(i)).onfail(fh);
+                } else {
+                    ph(i)(d);
+                }
+            }
+        }, 0);
+        return ret;
+    };
+
+})(µ, 'length');
